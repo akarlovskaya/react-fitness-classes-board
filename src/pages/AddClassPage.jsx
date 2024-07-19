2a8f8706bcc269b1c3a7b6a299a5f8049bba3419
Anna Karlovskaya <annakarlovskaya@Annas-MacBook-Air.local>
2024-07-19 14:39:44 -0700

    add state to inputs except checkboxes

--- src/pages/AddClassPage.jsx
+++ src/pages/AddClassPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { useState } from 'react'; 
 
 const AddClassPage = () => {
+    const [title, setTitle] = useState('');
+    const [type, setType] = useState('');
+    const [description, setDescription] = useState('');
+    const [time, setTime] = useState('');
+    const [days, setDays] = useState('');
+    const [fee, setFee] = useState(null);
+    const [place, setPlace] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [region, setRegion] = useState('');
+    const [zipcode, setZipcode] = useState('');
+    const [instructorName, setInstructorName] = useState('');
+    const [instructorDescription, setInstructorDescription] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
+    const [contactPhone, setContactPhone] = useState('');
+    const [paymentOptions, setPaymentOptions] = useState('');
+
   return (
     <section className="bg-indigo-50">
         <div className="container m-auto max-w-2xl py-24">
@@ -16,7 +34,10 @@ const AddClassPage = () => {
                     id="type"
                     name="type"
                     className="border rounded w-full py-2 px-3"
-                    required>
+                    required
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    >
                     <option value="Drop-In">Drop-In</option>
                     <option value="Session">Session</option>
                 </select>
@@ -33,6 +54,8 @@ const AddClassPage = () => {
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="E.g. Cardio Dance"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
             />
             </div>
 
@@ -45,7 +68,9 @@ const AddClassPage = () => {
                 name="description"
                 className="border rounded w-full py-2 px-3"
                 rows="5"
-                placeholder="Tell what to expect from your classes - e.g. goal of the class, duration, what participants should bring and wear, if any eguipment will be used in your class. Note: Make your first sentence as informative and catchy as possible, as only the first 130 characters will be visible on the Home Page. The full description will be visible on the Class Details page.">
+                placeholder="Tell what to expect from your classes - e.g. goal of the class, duration, what participants should bring and wear, if any eguipment will be used in your class. Note: Make your first sentence as informative and catchy as possible, as only the first 130 characters will be visible on the Home Page. The full description will be visible on the Class Details page."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}>
                 </textarea>
             </div>
 
@@ -59,6 +84,8 @@ const AddClassPage = () => {
                 name="time"
                 className="border rounded w-full py-2 px-3 mb-2"
                 required
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
             />
             </div>
 
@@ -70,7 +97,7 @@ const AddClassPage = () => {
                   <input id="monday" name="monday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="monday" class="font-medium text-gray-900">Monday</label>
+                  <label htmlFor="monday" class="font-medium text-gray-900">Monday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -78,7 +105,7 @@ const AddClassPage = () => {
                   <input id="tuesday" name="tuesday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="tuesday" class="font-medium text-gray-900">Tuesday</label>
+                  <label htmlFor="tuesday" class="font-medium text-gray-900">Tuesday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -86,7 +113,7 @@ const AddClassPage = () => {
                   <input id="wednesday" name="wednesday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="wednesday" class="font-medium text-gray-900">Wednesday</label>
+                  <label htmlFor="wednesday" class="font-medium text-gray-900">Wednesday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -94,7 +121,7 @@ const AddClassPage = () => {
                   <input id="thursday" name="thursday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="thursday" class="font-medium text-gray-900">Thursday</label>
+                  <label htmlFor="thursday" class="font-medium text-gray-900">Thursday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -102,7 +129,7 @@ const AddClassPage = () => {
                   <input id="friday" name="friday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="friday" class="font-medium text-gray-900">Friday</label>
+                  <label htmlFor="friday" class="font-medium text-gray-900">Friday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -110,7 +137,7 @@ const AddClassPage = () => {
                   <input id="saturday" name="saturday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="saturday" class="font-medium text-gray-900">Saturday</label>
+                  <label htmlFor="saturday" class="font-medium text-gray-900">Saturday</label>
                 </div>
               </div>
               <div class="relative flex gap-x-3">
@@ -118,39 +145,25 @@ const AddClassPage = () => {
                   <input id="sunday" name="sunday" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 </div>
                 <div class="text-sm leading-6">
-                  <label for="sunday" class="font-medium text-gray-900">Sunday</label>
+                  <label htmlFor="sunday" class="font-medium text-gray-900">Sunday</label>
                 </div>
               </div>
             </div>
           </fieldset>
-
-            {/* <div className="mb-4">
-            <label htmlFor="type" className="block text-gray-700 font-bold mb-2">Day</label>
-            <select
-                id="day"
-                name="day"
-            className="border rounded w-full py-2 px-3"required>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-                <option value="Saturday">Saturday</option>
-                <option value="Sunday">Sunday</option>
-            </select>
-            </div> */}
 
             <div className="mb-4">
             <label
                 htmlFor="fee"
                 className="block text-sm font-semibold leading-6 text-gray-900 mb-2">Fee</label>
             <input
-                type="text"
+                type="number"
                 id="fee"
                 name="fee"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Price for One Class or Session"
                 required
+                value={fee}
+                onChange={(e) => setFee(e.target.value)}
             />
             </div>
             </fieldset>
@@ -167,7 +180,9 @@ const AddClassPage = () => {
                         name='place'
                         className='border rounded w-full py-2 px-3 mb-2'
                         placeholder='E.g., name of a Gym or Studio'
-                        required           
+                        required
+                        value={place}
+                        onChange={(e) => setPlace(e.target.value)}         
                     />
 
                     <label htmlFor="address" className='block text-gray-700 font-bold mb-2'>
@@ -178,7 +193,9 @@ const AddClassPage = () => {
                         id='address'
                         name='address'
                         className='border rounded w-full py-2 px-3 mb-2'
-                        required           
+                        required
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}         
                     />
 
                     <label htmlFor="city" className='block text-gray-700 font-bold mb-2'>
@@ -189,7 +206,9 @@ const AddClassPage = () => {
                         id='city'
                         name='city'
                     className='border rounded w-full py-2 px-3 mb-2'
-                        required           
+                        required
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}       
                     />
 
                     <label htmlFor="region" className='block text-gray-700 font-bold mb-2'>
@@ -200,75 +219,123 @@ const AddClassPage = () => {
                         id='region'
                         name='region'
                         className='border rounded w-full py-2 px-3 mb-2'
-                        required           
+                        required
+                        value={region}
+                        onChange={(e) => setRegion(e.target.value)}     
                     />
-                    <label htmlFor="postal-code" className='block text-gray-700 font-bold mb-2'>
+                    <label htmlFor="zipcode" className='block text-gray-700 font-bold mb-2'>
                         ZIP / Postal code
                     </label>
                     <input
                         type='text'
-                        id='postal-code'
-                        name='postal-code'
+                        id='zipcode'
+                        name='zipcode'
                         className='border rounded w-full py-2 px-3 mb-2'
-                        required           
+                        required
+                        value={zipcode}
+                        onChange={(e) => setZipcode(e.target.value)}           
                     />
-
                 </div>
             </fieldset>
 
-
             <fieldset>
-            <legend className="font-semibold uppercase mb-2 mt-8">Instructor Info</legend>
-            <div className="mb-4">
-            <label htmlFor="instructor" className="block text-gray-700 font-bold mb-2">Name</label>
-            <input
-                type="text"
-                id="instructor"
-                name="instructor"
-            className="border rounded w-full py-2 px-3"/>
-            </div>
+                <legend className="font-semibold uppercase mb-2 mt-8">Instructor Info</legend>
+                <div className="mb-4">
+                <label htmlFor="instructorName" className="block text-gray-700 font-bold mb-2">Name</label>
+                <input
+                    type="text"
+                    id="instructorName"
+                    name="instructorName"
+                    className="border rounded w-full py-2 px-3"
+                    value={instructorName}
+                    onChange={(e) => setInstructorName(e.target.value)} 
+                />
+                </div>
 
-            <div className="mb-4">
-            <label
-                htmlFor="about_instructor"
-            className="block text-gray-700 font-bold mb-2"
-                >About You</label>
-            <textarea
-                id="about_instructor"
-                name="about_instructor"
-            className="border rounded w-full py-2 px-3"
-                rows="4"
-                placeholder="Tell a bit about yourself - experience, what moves you?"
-            ></textarea>
-            </div>
+                <div className="mb-4">
+                <label
+                    htmlFor="instructorDescription"
+                    className="block text-gray-700 font-bold mb-2">About You</label>
+                <textarea
+                    id="instructorDescription"
+                    name="instructorDescription"
+                    className="border rounded w-full py-2 px-3"
+                    rows="4"
+                    placeholder="Tell a bit about yourself - experience, what moves you?"
+                    value={instructorDescription}
+                    onChange={(e) => setInstructorDescription(e.target.value)} 
+                ></textarea>
+                </div>
 
-            <div className="mb-4">
-            <label
-                htmlFor="contact_email"
-            className="block text-gray-700 font-bold mb-2">Contact Email</label>
-            <input
-                type="email"
-                id="contact_email"
-                name="contact_email"
-            className="border rounded w-full py-2 px-3"
-                placeholder="You email address"
-                required
-            />
-            </div>
-            <div className="mb-4">
-            <label
-                htmlFor="contact_phone"
-            className="block text-gray-700 font-bold mb-2">Contact Phone</label>
-            <input
-                type="tel"
-                id="contact_phone"
-                name="contact_phone"
-            className="border rounded w-full py-2 px-3"
-                placeholder="Add phone number. Optional"
-            />
-            </div>
+                <div className="mb-4">
+                <label
+                    htmlFor="contactEmail"
+                    className="block text-gray-700 font-bold mb-2">Contact Email</label>
+                <input
+                    type="email"
+                    id="contactEmail"
+                    name="contactEmail"
+                    className="border rounded w-full py-2 px-3"
+                    placeholder="You email address"
+                    required
+                    value={contactEmail}
+                    onChange={(e) => setContactEmail(e.target.value)} 
+                />
+                </div>
+
+                <div className="mb-4">
+                <label
+                    htmlFor="contactPhone"
+                    className="block text-gray-700 font-bold mb-2">Contact Phone</label>
+                <input
+                    type="tel"
+                    id="contactPhone"
+                    name="contactPhone"
+                    className="border rounded w-full py-2 px-3"
+                    placeholder="Add phone number. Optional"
+                    value={contactPhone}
+                    onChange={(e) => setContactPhone(e.target.value)} 
+                />
+                </div>
             </fieldset>
 
+            <fieldset>
+            <legend class="block text-sm font-semibold leading-6 text-gray-900 mb-2">Payment Options</legend>
+            <div class="mb-4">
+              <div class="relative flex gap-x-3">
+                <div class="flex h-6 items-center">
+                  <input id="etransfer" name="etransfer" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
+                </div>
+                <div class="text-sm leading-6">
+                  <label htmlFor="etransfer" class="font-medium text-gray-900">E-transfer</label>
+                </div>
+              </div>
+              <div class="relative flex gap-x-3">
+                <div class="flex h-6 items-center">
+                  <input id="cash" name="cash" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
+                </div>
+                <div class="text-sm leading-6">
+                  <label htmlFor="cash" class="font-medium text-gray-900">Cash</label>
+                </div>
+              </div>
+              <div class="relative flex gap-x-3">
+                <div class="flex h-6 items-center">
+                  <input id="visa" name="visa" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
+                </div>
+                <div class="text-sm leading-6">
+                  <label htmlFor="visa" class="font-medium text-gray-900">Visa/Mastercard</label>
+                </div>
+              </div>
+              <div class="relative flex gap-x-3">
+                <div class="flex h-6 items-center">
+                  <input id="cheque" name="cheque" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
+                </div>
+                <div class="text-sm leading-6">
+                  <label htmlFor="cheque" class="font-medium text-gray-900">Cheque</label>
+                </div>
+              </div>
+              </div>
+          </fieldset>
 
             <div>
             <button
